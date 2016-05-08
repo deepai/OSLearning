117a2a9b5cc0e7c2f45eb446df407cc9ca2a3923
Debarshi Dutta <debarshi.dutta@research.iiit.ac.in>
2016-05-08 17:30:07 +0530

    added stack Initialisation in .bss for C programs.
    	modified:   loader.s

--- loader.s
+++ loader.s
@@ -4,6 +4,12 @@ MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
 FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
+KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
+
+section .bss:
+align 4
+kernel_stack:
+	resb KERNEL_STACK_SIZE
 
 section .text:                  ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
@@ -11,7 +17,8 @@ align 4                         ; the code must be 4 byte aligned
         dd FLAGS                    ; the flags,
         dd CHECKSUM                 ; and the checksum
 
-loader:                         ; the loader label (defined as entry point in linker script)
-	mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+loader:                         ; the loader label (defined as entry point in l 
+	mov esp,kernel_stack + KERNEL_STACK_SIZE
+                                                ;: stack (end of memory area)
 .loop:
         jmp .loop                   ; loop forever
